c61a08b50d4e3d3f42b4c2c4c7c0554fd886b69d
adey-stack <adey09133@gmail.com>
2026-06-13 20:36:52 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -613,7 +613,7 @@ app.mount("/uploads", StaticFiles(directory=str(UPLOAD_DIR)), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
